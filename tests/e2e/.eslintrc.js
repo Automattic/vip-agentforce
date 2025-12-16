3d27210d090e0fbfa539c5c781bb5c4803f83f99
Andrea Grassi <andrea.grassi@automattic.com>
2025-12-16 14:44:47 +0100

    update ESLint and Prettier configurations

--- tests/e2e/.eslintrc.js
+++ tests/e2e/.eslintrc.js
@@ -1,18 +1,20 @@
-require( '@automattic/eslint-plugin-wpvip/init' );
+require('@automattic/eslint-plugin-wpvip/init');
 
 module.exports = {
-    parserOptions: {
-        ecmaVersion: 2021,
-        project: [__dirname  + "/tsconfig.json"],
-    },
-    extends: [
-        'plugin:@automattic/wpvip/recommended',
-        'plugin:playwright/playwright-test',
-        'plugin:deprecation/recommended',
-    ],
-    ignorePatterns: [ 'bin/**/*', '*.js' ],
-    rules: {
-        '@typescript-eslint/no-non-null-assertion': 'off',
-    },
-    root: true,
+	parserOptions: {
+		ecmaVersion: 2021,
+		project: [__dirname + '/tsconfig.json'],
+	},
+	extends: [
+		'plugin:@automattic/wpvip/recommended',
+		'plugin:playwright/playwright-test',
+		'plugin:deprecation/recommended',
+	],
+	ignorePatterns: ['bin/**/*', '*.js'],
+	rules: {
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'space-in-parens': 'off',
+		'comma-dangle': 'off',
+	},
+	root: true,
 };
